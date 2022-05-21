a3120d7ac111130d41f01fe7fddb31971b9d5622
Hiren Gondhiya <hgondhiya@woolworths.com.au>
2022-05-21 14:45:36 +1000

    lookup for base url from env file

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,11 @@
-import { createHash } from 'crypto';
+import { createHash } from "crypto";
 
-export function hashUrl(url: string): string {
-  return createHash('sha1').update(url).digest('base64');
+export function hashUrl(url: string, baseUrl?: string): string {
+  return createHash("sha1")
+    .update(
+      `${
+        baseUrl || process.env.BASE_URL || "https://www.woolworths.com.au"
+      }${url}`
+    )
+    .digest("base64");
 }
