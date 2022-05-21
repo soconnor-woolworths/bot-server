917c340f647360906c409b943ab077b4d7fd12db
Hiren Gondhiya <hgondhiya@woolworths.com.au>
2022-05-21 14:49:58 +1000

    connect to database and listen on port 3000

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectMongoDb } from './connect-db';
+import { Mongoose } from 'mongoose';
 
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
@@ -8,11 +10,29 @@ dotenv.config({
 const app = express();
 
 app.get('*', (req, res) => {
-    res.send(`This is a test web page! ${req.url}`);
-    // read look up table
+  res.send(`This is a test web page! ${req.url}`);
+  // read look up table
+});
 
-})
-
-app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
-})
+connectMongoDb()
+  .then((mongoose: Mongoose) => {
+    mongoose.connection.db
+      .listCollections()
+      .toArray(function (err, collections) {
+        if (err) {
+          console.log(err);
+        } else if (collections) {
+          console.log('Available Collections', {
+            collectionNames: collections.map(({ name }) => name),
+          });
+        }
+      });
+    app
+      .listen(3000, () => {
+        console.log('The application is listening on port 3000!');
+      })
+      .on('close', () => mongoose.disconnect());
+  })
+  .catch((err) => {
+    console.error({ err });
+  });
